0195882cbd8290cd447376fc0821748b61ca5c65
leenahmad <leenkaraja24@gmail.com>
2022-04-10 12:45:43 +0300

    finish index amd server.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+'use strict';
+
 // this gile will run our server and initiate the database
+
+require('dotenv').config()
+
+const server = require('./src/server')
+
+const {db} = require('./src/model/index')
+
+db.sync().then(() => {
+    server.start(process.env.PORT || 3000)
+}).catch(console.error);
+
