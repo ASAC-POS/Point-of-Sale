c44b84285706325649df393eb70974383041e9c1
JalalHasan-22 <jhasanj8@gmail.com>
2022-04-11 11:03:14 +0300

    store-user relation

--- src/model/index.js
+++ src/model/index.js
@@ -1,40 +1,48 @@
- // this will initiate sequelize
+// this will initiate sequelize
 
 //this file is for  preparing the connection between the server and  postgresql server .
 
-const{Sequelize,DataTypes}=require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-const users=require('./user');
-const stores =require('./stores');
-const receipts =require('./receipts');
+const users = require("./user");
+const stores = require("./stores");
+const receipts = require("./receipts");
 
+const POSTGRES_URL =
+  process.env.NODE_ENV === "test" ? "sqlite:memory:" : process.env.DATABASE_URL;
 
+let sequelizeOptions =
+  process.env.NODE_ENV === "production"
+    ? {
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      }
+    : {};
 
+const sequelize = new Sequelize(POSTGRES_URL, sequelizeOptions);
 
-const POSTGRES_URL  = process.env.NODE_ENV === 'test' ? 'sqlite:memory:' : process.env.DATABASE_URL; 
+// Relations
 
-let sequelizeOptions = process.env.NODE_ENV === 'production' ? {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
-  }
-} : {};
+// Building the models
+const userModel = users(sequelize, DataTypes);
+const storesModel = stores(sequelize, DataTypes);
+const receiptsModel = receipts(sequelize, DataTypes);
 
-const sequelize = new Sequelize(POSTGRES_URL , sequelizeOptions);
+// Store one-to-many relation with the users
 
+storesModel.hasMany(userModel, { foreignKey: "storeID", sourceKey: "id" });
+userModel.belongsTo(storesModel, { foreignKey: "storeID", targetKey: "id" });
 
-
-
-
-
-module.exports={
-    db :sequelize, //for connection ,we will use it the index.js 
-    users: users(sequelize,DataTypes),
-    stores : stores(sequelize,DataTypes),
-    receipts:receipts(sequelize,DataTypes)
-}
+module.exports = {
+  db: sequelize, //for connection ,we will use it the index.js
+  users: userModel,
+  stores: storesModel,
+  receipts: receiptsModel,
+};
--- src/model/receipts.js
+++ src/model/receipts.js
@@ -1,29 +1,25 @@
-// this will include receipts table model => for the receipts table 
-const receipts =(sequelize,DataTypes)=> sequelize.define('receipts',{
-
-
-
-    totalPrice:{
-        type:DataTypes.STRING,
+// this will include receipts table model => for the receipts table
+const receipts = (sequelize, DataTypes) =>
+  sequelize.define("receipts", {
+    totalPrice: {
+      type: DataTypes.STRING,
     },
 
-    quantityNumber :{
-        type:DataTypes.STRING,
+    quantityNumber: {
+      type: DataTypes.STRING,
     },
 
-     userID:{
-        type:DataTypes.INTEGER,
-        allowNull:false 
-     },
-     paymentDate: {
-        type: Sequelize.DATE,
-      },
+    userID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    paymentDate: {
+      type: DataTypes.DATE,
+    },
 
-      itemsSold :{
-        type: Sequelize.ARRAY,
-      }
+    // itemsSold: {
+    //   type: DataTypes.ARRAY,
+    // },
+  });
 
-    })
-    
-    
-    module.exports=receipts;
+module.exports = receipts;
